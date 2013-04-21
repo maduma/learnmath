59b7f50c18198810a897cbd24b0735a03243f9d6
Stéphane Nsakala <maduma@pt.lu>
2013-04-21 15:10:10 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,52 @@
+﻿var views = [ 'auth', 'menu', 'conf', 'play', 'report'];
+var currentView = 'auth';
+var uid = '';
+var uids = [];
+
+function changeView(view) {
+    views.forEach(function(e, i, a) { if (e !== view) { $('div#' + e).hide(); } });
+    $('div#' + view).fadeIn();
+    currentView = view;
+    console.log("changeView: " + view);
+}
+
+function ctrlAuth() {
+    $('div#auth li').each(function(i) {
+        uid = $(this).attr('id');
+        uids.push(u);
+        $(this).click(function(){
+            $('span#uid').html(uid);
+            changeView('menu');
+        });
+    });
+    console.log("Uids: " + uids);
+}
+
+function ctrlMenu() {
+    $('div#menu li').each(function(i) {
+        var v = $(this).attr('id');
+        $(this).click(function(){
+            changeView(v);
+        });
+    });
+}
+
+/*
+ * Main
+ */
+
+$( document ).ready(function() {
+    console.log("Document Ready.");
+    //views.forEach(function(e, i, a) { setTimeout( function() { changeView(e); }, 2000 * (i + 1)); });
+    
+    /*
+     * controllers
+     */
+    ctrlAuth();
+    ctrlMenu();
+    
+    /*
+     * Authenticate User
+     */
+    changeView('auth');
+});
