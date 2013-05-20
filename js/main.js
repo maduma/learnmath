627e1e9c61b0e85f899ae712ada80b7cabf216e4
maduma <maduma@pt.lu>
2013-05-20 22:40:58 +0200

    start using jquery mobile

--- js/main.js
+++ js/main.js
@@ -3,145 +3,289 @@ var currentView = 'auth';
 var uid = '';
 var uids = [];
 var works = {
-	'add' : ['1:5', '6:10', '11:15', '1:20'],
-	'sub' : ['1:5', '6:10', '11:15', '1:20'],
-	'mul' : ['1:5', '6', '7', '8', '9', '10']
+  'add' : ['1:5', '1:10', '1:15', '1:20']
+}
+/*
+  'sub' : ['1:5', '6:10', '11:15', '1:20'],
+  'mul' : ['1:5', '6', '7', '8', '9', '10']
 };
+*/
+var exercise;
 var op = '';
 var range = '';
-var countdown = 300;
+var countdown = 0;
+var exTime = 10;
 var countdownId = 0;
 var correct = 0;
+var wrong = 0;
 var total = 0;
 var solution = '';
 var answer = '';
 var inGame = false;
+var oriStat = 1000;
+
+/*
+ * Works
+ */
+ 
+function AddExercise(min, max) {
+  this.allQ = [];
+  for (var i = min; i < max; i++) {
+    for (var j = min; j < max; j++) {
+      this.allQ.push({
+        question: i + ' + ' + j,
+        solution: i + j,
+        correct: 0,
+        wrong: 0
+      });
+    }
+  }
+  this.min = min;
+  this.max = max;
+  this.currentQ;
+}
+
+function getProb(q) {
+  if (q.correct != 0) {
+    return Math.round(oriStat / (q.correct + 1));
+  }
+  return oriStat;
+}
+
+AddExercise.prototype.load = function() {
+  for (var q in this.allQ) {
+    var correct = localStorage.getItem(uid + ':' + this.allQ[q].question + '_correct');
+    if (correct) {
+      console.log('load ', this.allQ[q].question, '_correct');
+      this.allQ[q].correct = parseInt(correct);
+    }
+    var wrong = localStorage.getItem(uid + ':' + this.allQ[q].question + '_wrong');
+    if (wrong) {
+      console.log('load ', this.allQ[q].question, '_wrong');
+      this.allQ[q].wrong = parseInt(wrong);
+    }
+  }
+}
+
+AddExercise.prototype.save = function() {
+  for (var q in this.allQ) {
+    localStorage.setItem(uid + ':' + this.allQ[q].question + '_correct', this.allQ[q].correct);
+    localStorage.setItem(uid + ':' + this.allQ[q].question + '_wrong', this.allQ[q].wrong);
+  }
+}
+
+AddExercise.prototype.nextQuestion = function() {
+  var stat = Math.floor(Math.random() * this.totalStat());
+  console.log(stat);
+  var q = this.find(stat);
+  console.log(q);
+  this.currentQ = q;
+}
+
+AddExercise.prototype.totalStat = function() {
+  var totalS = 0;
+  for (var q in this.allQ) {
+    totalS = totalS + getProb(this.allQ[q]);
+  }
+  return totalS;
+}
+
+AddExercise.prototype.find = function(s) {
+  var totalS = 0;  
+  for (var q in this.allQ) {
+    totalS = totalS + getProb(this.allQ[q]);
+    if (totalS > s) {
+      return this.allQ[q];
+    }
+  }
+}
+
+AddExercise.prototype.toHtml = function() {
+  var index = 0;
+  var html = [];
+  html.push('<table class="report">');
+  html.push('<tr><td>+</td>');
+  for (var i = this.min; i < this.max; i++) {
+    html.push('<td>' + i + '</td>');
+  }
+  html.push('</tr>');
+  for (var i = this.min; i < this.max; i++) {
+    html.push('<tr><td>' + i + '</td>');
+    for (var j = this.min; j < this.max; j++) {
+      html.push('<td>');
+      html.push(this.allQ[index].correct + ':' + this.allQ[index].wrong);
+      index++;
+      html.push('</td>');
+    }
+    html.push('</tr>');
+  }
+  html.push('</table>');
+  return html.join('');
+}
+
+AddExercise.prototype.toHtmlStat = function() {
+  var index = 0;
+  var html = [];
+  html.push('<table class="reportStat">');
+  html.push('<tr><td>+</td>');
+  for (var i = this.min; i < this.max; i++) {
+    html.push('<td>' + i + '</td>');
+  }
+  html.push('</tr>');
+  for (var i = this.min; i < this.max; i++) {
+    html.push('<tr><td>' + i + '</td>');
+    for (var j = this.min; j < this.max; j++) {
+      html.push('<td>');
+      html.push(getProb(this.allQ[index]));
+      index++;
+      html.push('</td>');
+    }
+    html.push('</tr>');
+  }
+  html.push('</table>');
+  return html.join('');
+}
+
+/*
+ * UI
+ */
 
 function changeView(view) {
-	views.forEach(function(e, i, a) { if (e !== view) { $('div#' + e).hide(); } });
-	$('div#' + view).fadeIn();
-	currentView = view;
-	console.log("changeView: " + view);
-}
-
-function ctrlCommon() {
-	$('span#home').click(function(){ changeView('menu'); });
-}
-
-function ctrlAuth() {
-	$('div#auth li').each(function(i) {
-		uids.push($(this).attr('id'));
-		$(this).click(function(){
-			uid = $(this).attr('id');
-			$('span#uid').html('Bonjour ' + uid);
-			changeView('conf');
-		});
-	});
-	console.log("Uids: " + uids);
-}
-
-function ctrlMenu() {
-	$('div#menu li').each(function(i) {
-		var v = $(this).attr('id');
-		$(this).click(function(){
-			changeView(v);
-		});
-	});
+  views.forEach(function(e, i, a) { if (e !== view) { $('div#' + e).hide(); } });
+  $('div#' + view).fadeIn();
+  currentView = view;
+  console.log("changeView: " + view);
 }
 
 function updateRange() {
-	op = $('div#conf select#op').val();
-	var elem = $('div#conf select#range');
-	elem.empty();
-	var choices = works[op];
-	choices.forEach(function(e, i, a) {
-		elem.append('<option value="' + e + '">' + e + '</option>');
-	});
-	elem.show();
-	console.log('Op: ' + op);
-	console.log('Choices: ' + choices);
-}
-
-function ctrlConf() {
-	updateRange();
-	$('div#conf select#op').change(function(){ updateRange(); });
-	$('button#play').click(function(){ changeView('play'); });
+  op = $('div#conf select#op').val();
+  var elem = $('div#conf select#range');
+  elem.empty();
+  var choices = works[op];
+  choices.forEach(function(e, i, a) {
+    elem.append('<option value="' + e + '">' + e + '</option>');
+  });
+  elem.show();
+  console.log('Op: ' + op);
+  console.log('Choices: ' + choices);
 }
 
 function countdownStep() {
-	countdown = countdown - 1;
-	$('div#timer').html(countdown);
-	if (countdown == 0) { 
-		stopExercice();
-	}
+  countdown = countdown - 1;
+  $('div#timer').html(countdown);
+  if (countdown == 0) { 
+    stopExercice();
+    exercise.save();
+    $('div#resultTable').html(exercise.toHtml());
+    $('div#statTable').html(exercise.toHtmlStat());
+    changeView('report');
+  }
 }
 
 function startExercice() {
-	console.log('Start');
-	$('button#start').unbind('click');
-	$('button#start').click(stopExercice);
-	$('button#start').html('Stop');
-	countdownId = setInterval(countdownStep, 1000);
-	nextQuestion();
-	inGame = true;
+  countdown = exTime;
+  console.log('Start');
+  $('div#timer').html(countdown);
+  $('button#start').unbind('click');
+  $('button#start').click(stopExercice);
+  $('button#start').html('Stop');
+  countdownId = setInterval(countdownStep, 1000);
+  exercise = new AddExercise(1, 5);
+  exercise.load();
+  nextQuestion();
+  inGame = true;
 }
 
 function stopExercice() {
-	console.log('Stop');
-	$('button#start').unbind('click');
-	clearInterval(countdownId);
-	$('button#start').click(startExercice);
-	$('button#start').html('Start');
-	inGame = false;
-}
-
-function ctrlPlay() {
-	$('div#timer').html(countdown);
-	$('button#start').click(startExercice);
-}
-
-function nextQuestion(result) {
-	console.log('result: ' + result);
-	if (result == 'correct') {
-		correct = correct + 1;
-		if (correct == 3) {
-			stopExercice();
-			changeView('report');
-		}
-	}
-	$('span#question').html('1232 + 12');
-	$('span#answer').html('?');
-    solution = 1244 + '';
-	answer = '';
+  console.log('Stop');
+  $('button#start').unbind('click');
+  clearInterval(countdownId);
+  $('button#start').click(startExercice);
+  $('button#start').html('Start');
+  inGame = false;
 }
 
 function proceedAnswer(result) {
-	console.log("answer: " + result);
-	inGame = false;
+  console.log("answer: " + result);
+  inGame = false;
     $('span#answer').fadeOut(function() {
-		$(this).html(result);
-		$(this).fadeIn(function() {
-			inGame = true;
-			nextQuestion(result);
-		});
-	});	
+    $(this).html(result);
+    $(this).fadeIn(function() {
+      inGame = true;
+      nextQuestion(result);
+    });
+  });  
 }
 
-function ctrlNumpad() {
-	$('div#numpad button').click(function() {
-			if (!inGame) { return; }
-			digit = $(this).attr('id').substring(2);
-			answer = answer + digit;
-			$('span#answer').html(answer);
-			if (solution == answer) { 
-				proceedAnswer('correct');
-			}
-			if (solution.search(answer) == 0) {
-				return;
-			} else {
-				proceedAnswer('wrong');
-			}
-	});
+function nextQuestion(result) {
+  console.log('result: ' + result);
+  if (result == 'correct') {
+    correct++;
+    exercise.currentQ.correct++;
+  } else if (result == 'wrong') {
+    exercise.currentQ.wrong++;
+    wrong++;
+  }
+  exercise.nextQuestion();
+  $('span#question').html(exercise.currentQ.question);
+  $('span#answer').html('?');
+  solution = exercise.currentQ.solution + '';
+  answer = '';
+}
+
+/*
+ * Controllers
+ */
+
+function ctrlCommon() {
+  $('span#home').click(function(){ changeView('menu'); });
+}
+
+function ctrlAuth() {
+  $('div#auth li').each(function(i) {
+    uids.push($(this).attr('id'));
+    $(this).click(function(){
+      uid = $(this).attr('id');
+      $('span#uid').html('Bonjour ' + uid);
+      changeView('conf');
+    });
+  });
+  console.log("Uids: " + uids);
+}
+
+function ctrlMenu() {
+  $('div#menu li').each(function(i) {
+    var v = $(this).attr('id');
+    $(this).click(function(){
+      changeView(v);
+    });
+  });
+}
+
+function ctrlConf() {
+  updateRange();
+  $('div#conf select#op').change(function(){ updateRange(); });
+  $('button#play').click(function(){ changeView('play'); });
+}
+
+function ctrlPlay() {
+  $('button#start').click(startExercice);
+}
+
+function bindNumpad() {
+  $('div#numpad button').click(function() {
+      digit = $(this).attr('id').substring(2);
+      answer = answer + digit;
+      $('span#answer').html(answer);
+      if (solution == answer) { 
+        proceedAnswer('correct');
+      }
+      if (solution.search(answer) == 0) {
+        return;
+      } else {
+        proceedAnswer('wrong');
+      }
+  });
 }
 
 /*
@@ -149,21 +293,16 @@ function ctrlNumpad() {
  */
 
 $( document ).ready(function() {
-	console.log("Document Ready.");
-	//views.forEach(function(e, i, a) { setTimeout( function() { changeView(e); }, 2000 * (i + 1)); });
+  console.log("Document Ready.");
+  
+  // Bind Ctrls
+  ctrlCommon();
+  ctrlAuth();
+  ctrlMenu();
+  ctrlConf();
+  ctrlPlay();
+  ctrlNumpad();
 
-	/*
-	 * controllers
-	 */
-	ctrlCommon();
-	ctrlAuth();
-	ctrlMenu();
-	ctrlConf();
-	ctrlPlay();
-    ctrlNumpad();
-
-	/*
-	 * Authenticate User
-	 */
-	changeView('auth');
+  // Authenticate User
+  changeView('auth');
 });
---
+++ js/mobile.js
@@ -0,0 +1,128 @@
+﻿var uid;
+var op;
+var exe;
+var numpad;
+
+/*
+ * Binding
+ */
+
+function bindAuth() {
+  $('input.auth').click( function(event) {
+    uid = $(this).val();
+    console.log('uid:', uid);
+    $.mobile.changePage('#confOp', { transition: 'slidedown'});
+  });
+}
+
+function bindConfOp() {
+  $('input.confOp').click( function(event) {
+    op = $(this).val();
+    console.log('op:', op);
+    $.mobile.changePage('#addExe', { transition: 'slidedown'});
+  });
+}
+
+function bindAddExe() {
+  $('input.addExe').click( function(event) {
+    exe = $(this).val();
+    console.log('addExe:', exe);
+    $.mobile.changePage('#play', { transition: 'slidedown'});
+  });
+}
+
+function bindPlay() {
+  $('button#start').click(function() {
+  });
+}
+
+function AddExercise(min, max) {
+  this.allQ = [];
+  for (var i = min; i < max; i++) {
+    for (var j = min; j < max; j++) {
+      this.allQ.push({
+        question: i + ' + ' + j,
+        solution: i + j,
+        correct: 0,
+        wrong: 0
+      });
+    }
+  }
+  this.min = min;
+  this.max = max;
+  this.currentQ;
+}
+
+function Numpad(handler) {
+  this.question = '9 + 7';
+  this.answer = '';
+  this.solution = '16';
+  this.handler = handler;
+  this.enabled = true;
+  
+  var that = this;
+  $('div#numpad button').click(function() {
+    if (!that.enabled) { return; }
+    var digit = $(this).attr('id').substring(1);
+    that.answer = that.answer + digit;
+    $('span#answer').html(that.answer);
+    if (that.solution == that.answer) {
+      that.enabled = false;
+      that.handler('correct');
+    }
+    if (that.solution.search(that.answer) == 0) {
+      return;
+    } else {
+      that.enabled = false;
+      that.handler('wrong');
+    }
+  });
+}
+
+function proceedAnswer(status) {
+  console.log(status);
+}
+
+/* 
+ * Main
+ */
+ 
+$( document ).delegate('#auth', 'pageinit', function() {
+  console.log('pageinit auth');
+});
+ 
+$( document ).delegate('#confOp', 'pageinit', function() {
+  console.log('pagechange confOp');
+  if (!uid) {
+    $.mobile.changePage('#auth');
+  }
+});
+
+$( document ).delegate('#addExe', 'pageinit', function() {
+  console.log('pagechagne addExe');
+  if (!uid) {
+    $.mobile.changePage('#auth');
+  } else if (!op) {
+    $.mobile.changePage('#confOp');
+  }
+});
+
+$( document ).delegate('#play', 'pageinit', function() {
+  console.log('pagechange play');
+  if (!uid) {
+    $.mobile.changePage('#auth');
+  } else if (!op) {
+    $.mobile.changePage('#confOp');
+  } else if (!exe) {
+    $.mobile.changePage('#addExe');
+  }
+});
+
+$( document ).ready(function() {
+  console.log("Document Ready.");
+  bindAuth();
+  bindConfOp();
+  bindAddExe();
+  bindPlay();
+  numpad = new Numpad(proceedAnswer);
+});
